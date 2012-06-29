d5a1af834e799a28643e1f32945d604638787771
tiny657 <tiny657@gmail.com>
2012-06-29 13:59:50 +0900

    [#1] JUnit에서 Transactional 처리 추가

--- src/test/java/com/tiny/common/CommonTest.java
+++ src/test/java/com/tiny/common/CommonTest.java
@@ -4,11 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-		"classpath:applicationContext.xml",
-		"classpath:jdbc-context.xml" })
+@ContextConfiguration(locations = { "classpath:applicationContext-test.xml" })
+@Transactional
 public class CommonTest {
 	@Test
 	public void test() {
