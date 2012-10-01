d19fb31f67a612d65df7b2c74b6b25d35a61beff
tiny657 <tiny657@gmail.com>
2012-10-02 01:12:06 +0900

    [#5] XssFilter 보강

--- src/main/java/com/tiny/common/util/XssFilter.java
+++ src/main/java/com/tiny/common/util/XssFilter.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Component;
 public class XssFilter {
 	public String doFilter(String inputString) {
 		String escapedHtml = StringEscapeUtils.escapeHtml(inputString);
-		return escapedHtml.replace("\r\n", "<br/>").replace(" ", "&nbsp");
+		return escapedHtml.replace("\r\n", "<br/>").replace("\n", "<br/>").replace(" ", "&nbsp");
 	}
 }
